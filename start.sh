b94cb8c7310851fdc9f85e3dcd1a2673d45bff39
DaMatrix <joey.rabil@gmail.com>
2018-03-18 16:24:26 +0100

    update things

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@ do
     cd src/main/java/net/twoptwoe/mobplugin/
     git pull origin MobPlugin
     cd ../../../../../..
-    mvn clean compile exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -Xmx6G -Xms6G cn.nukkit.Nukkit"
+    mvn compile exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -Xmx6G -Xms6G cn.nukkit.Nukkit"
 done
