f43bc6b42152ff714ce529cb5e51d7a11e7eb28b
DaMatrix <joey.rabil@gmail.com>
2018-03-24 03:03:00 +0100

    Fix #27

--- src/main/java/net/daporkchop/mcpe/UtilsPE.java
+++ src/main/java/net/daporkchop/mcpe/UtilsPE.java
@@ -56,4 +56,27 @@ public class UtilsPE {
             }
         }, 432000);
     }
+
+    /**
+     * Returns a random number between min (inkl.) and max (excl.) If you want a number between 1 and 4 (inkl) you need to call rand (1, 5)
+     *
+     * @param min min inklusive value
+     * @param max max exclusive value
+     * @return
+     */
+    public static int rand(int min, int max) {
+        if (min == max) {
+            return max;
+        }
+        return min + random.nextInt(max - min);
+    }
+
+    /**
+     * Returns random boolean
+     *
+     * @return a boolean random value either <code>true</code> or <code>false</code>
+     */
+    public static boolean rand() {
+        return random.nextBoolean();
+    }
 }
