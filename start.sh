862659d015badc524856a28857ab7fc86f45694c
DaMatrix <joey.rabil@gmail.com>
2019-09-15 01:30:18 +0200

    try to make this work a LOT faster

--- start.sh
+++ start.sh
@@ -5,8 +5,6 @@ do
     echo "Press Ctrl+C to stop"
     sleep 3
     git pull
-    cd src/main/java/net/twoptwoe/mobplugin/
-    git pull origin MobPlugin
-    cd ../../../../../..
-    ./mvnw compile exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -XX:+UseG1GC -Xmx4G -Xms256M cn.nukkit.Nukkit"
+    wget https://jenkins.daporkchop.net/job/2p2e/job/Nukkit/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+    java -XX:+UseG1GC -Xmx3G -Xms256M -jar nukkit-1.0-SNAPSHOT.jar
 done
