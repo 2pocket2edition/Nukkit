b412ecfbc15e2af28d82d0c7962233a61c1dacc6
DaMatrix <joey.rabil@gmail.com>
2019-09-15 01:36:22 +0200

    delete maven wrapper

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ do
     echo "Press Ctrl+C to stop"
     sleep 3
     git pull
+    rm nukkit-1.0-SNAPSHOT.jar*
     wget https://jenkins.daporkchop.net/job/2p2e/job/Nukkit/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
     java -XX:+UseG1GC -Xmx3G -Xms256M -jar nukkit-1.0-SNAPSHOT.jar
 done
