2676dabf7116a9d17f5b247d9f27d865a9138585
DaMatrix <joey.rabil@gmail.com>
2019-09-15 10:43:35 +0200

    make restarting a LOT faster by downloading jar directly from the server rather than having to go through the internet
    LAN is fast

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ do
     sleep 3
     git pull
     rm nukkit-1.0-SNAPSHOT.jar*
-    wget https://jenkins.daporkchop.net/job/2p2e/job/Nukkit/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+    wget --no-check-certificate --header="Host: jenkins.daporkchop.net" https://192.168.1.120/job/2p2e/job/Nukkit/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
     bash launch.sh
 done
