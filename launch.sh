52d4403326189f6f4300b7ba5e55a126ff6b47cd
DaPorkchop_ <daporkchop@daporkchop.net>
2020-05-01 10:29:17 +0200

    bump max ram from 3GB to 8GB

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 wget --no-check-certificate --header="Host: jenkins.daporkchop.net" https://10.0.0.20/job/Minecraft/job/2p2e/job/NukkitX/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
 
-java -Xmx3G -Xms1G \
+java -Xmx8G -Xms1G \
 -XX:+UseG1GC \
 -Dsun.rmi.dgc.server.gcInterval=2147483646 \
 -XX:+UnlockExperimentalVMOptions \
