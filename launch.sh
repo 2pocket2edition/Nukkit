6bd5476c5b3b3f4a05abbdd540c2c1a680d6a6fb
DaPorkchop_ <daporkchop@daporkchop.net>
2020-07-13 12:07:17 +0200

    update launch args

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget --no-check-certificate --header="Host: jenkins.daporkchop.net" https://10.0.0.20/job/Minecraft/job/2p2e/job/NukkitX/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+wget -O nukkit-1.0-SNAPSHOT.jar --no-check-certificate --header="Host: jenkins.daporkchop.net" https://10.0.0.20/job/Minecraft/job/2p2e/job/NukkitX/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
 
-java -Xmx8G -Xms1G \
+java -Xmx1500M -Xms1G \
 -XX:+UseG1GC \
 -Dsun.rmi.dgc.server.gcInterval=2147483646 \
 -XX:+UnlockExperimentalVMOptions \
